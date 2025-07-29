29eccbd14c26988216b3f24e945824835a0cd87b
nh <nichohuaman16@gmail.com>
2025-07-28 23:02:44 -0500

    feat: fetch project list and start dashboard view implementation

--- src/api/ProjectAPI.ts
+++ src/api/ProjectAPI.ts
@@ -1,11 +1,25 @@
 import API from "@/lib/axios"
-import type { ProjectFormData } from "../types"
+import { dashboardProjectSchema, type ProjectFormData } from "../types"
 import { isAxiosError } from "axios"
 
 export async function createProject( formData : ProjectFormData) {
     try {
         const { data } = await API.post('/projects', formData)
         return data
+    } catch (error) {
+        if(isAxiosError(error) && error.response){
+            throw new Error(error.response.data.message)
+        }
+    }
+}
+
+
+
+export async function getProjects() {
+    try {
+        const { data } = await API.get('/projects')
+        const response = dashboardProjectSchema.safeParse(data.data)
+        return response.data
     } catch (error) {
         if(isAxiosError(error) && error.response){
             throw new Error(error.response.data.message)
