7ba40948cde777f43cc8d14402b8f9a2fa7e777f
nh <nichohuaman16@gmail.com>
2025-08-10 12:17:45 -0500

    feat(auth): add login and register views

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,6 +1,18 @@
 import { z } from "zod"
 
 
+// -- Auth Users -- //
+export const authSchema = z.object({
+    name: z.string(),
+    email: z.email(),
+    password: z.string(),
+    password_confirmation: z.string(),
+})
+
+type Auth = z.infer<typeof authSchema>
+export type UserLoginForm = Pick<Auth, 'email' | 'password'>
+export type UserRegistrationForm = Pick<Auth, 'name' | 'email' | 'password' | 'password_confirmation'>
+
 // -- Tasks -- //
 export const taskStatusSchema = z.enum(['pending', 'onHold', 'in-progress', 'completed']);
 export type TaskStatus = z.infer<typeof taskStatusSchema>
