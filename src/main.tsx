269047da94248f5d8fa7a803b8345fd4cb86622c
nh <nichohuaman16@gmail.com>
2025-10-11 17:35:04 -0500

    vercel.json

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import './index.css'
 import Router from './router'
-import { ToastContainer } from 'react-toastify'
+import { Slide, ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css';
 
 const queryClient = new QueryClient()
@@ -15,10 +15,18 @@ createRoot(document.getElementById('root')!).render(
     <QueryClientProvider client={queryClient}>
       <Router />
       <ReactQueryDevtools/>
-      <ToastContainer 
-        pauseOnHover={false} 
-        pauseOnFocusLoss={false} 
+      <ToastContainer
+        position="top-right"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
         draggable
+        pauseOnHover
+        theme="colored"
+        transition={Slide}
       />
     </QueryClientProvider>
   </StrictMode>,
